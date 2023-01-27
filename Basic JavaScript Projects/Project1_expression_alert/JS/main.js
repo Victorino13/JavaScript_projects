251f31e501688d2975e93a990c609ba18bc8eb20
Victorino13 <114184082+Victorino13@users.noreply.github.com>
2023-01-28 05:00:43 +0800

    assignment

--- Basic JavaScript Projects/Project1_expression_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expression_alert/JS/main.js
@@ -1,2 +1,22 @@
 window.alert("Hello World!");
 document.write('Hello World!');
+var A = "I am Victorino";
+document.write(A);
+var B = "I am 35 years old!"
+window.alert(B);
+document.write("\"Thank you for visiting my page\\website\"");
+document.write("\"Any fool can write code that a computer can understand."
++ "Good Programmers write code that humans can understand.\""
++ "-Martin Fowler");
+var C = "S" + "tring"
+document.write(C);
+var Family = "Palacio", Dad = "Choy", Son = "Teo", Mom = "Cielo";
+document.write(Son);
+document.write(8 + 8);
+function My_First_Function() {                                  //Defining a function and namint it
+    var str = "This is the button text!";                       //Defining a variable and giving it
+                                                                //a string value
+    document.getElementById("Button Text").innerHTML = str;     //Putting the value
+                                                                //of the variable into the HTML elementFromPoint
+                                                                //with the "Button_Text" id
+}
