62fa2a0e40690827ae0ed0ef27376d2a0a3c5855
Victorino13 <114184082+Victorino13@users.noreply.github.com>
2023-02-13 05:13:47 +0800

    commit

---
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,67 @@
+//Ternary Operators 
+function Ride_Function () {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+//Ternary Operators Challenge
+function Vote () {
+    var Age, Legal_age;
+    Age = document.getElementById("Age").value;
+    Legal_age = (Age < 18) ? "You are not old enough":"You are old enough";
+    document.getElementById("Voter").innerHTML = Legal_age + " to vote";
+}
+
+//Coding Assignment
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = 
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    " manufactured in " + Erik.Vehicle_Year;
+}
+
+//New keyword assignment
+function Shirt(Brand, Size, Color) {
+    this.Shirt_Brand = Brand;
+    this.Shirt_Size = Size;
+    this.Shirt_color = Color;
+}
+var Vic = new Shirt("Lacoste", "XL", "White");
+function Dress () {
+    document.getElementById("New_and_This").innerHTML = "Vic wears a " +
+    Vic.Shirt_color + "-colored " + Vic.Shirt_Brand + " Size " + Vic.Shirt_Size;
+}
+
+//Reserved keyword challenge
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+var True = new Driver("Vic", "Palacio", "34", "brown");
+function haul () {
+    document.getElementById("Fedex").innerHTML = "The driver is " +
+    True.firstname + True.lastName;
+}
+
+//Nested function assignment
+function count_Function () {
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+            Plus_one();
+            return Starting_point;
+    }
+}
